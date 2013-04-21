12980db5329eb801b90d0966b02b5e5177d1741a
Rafael Veronezi <developer@github.com>
2013-04-21 13:11:26 -0300

    Adds the workspace and stubs for the Problem Set 4

--- Assignments/Assignment3/StockCutting/main.cpp
+++ Assignments/Assignment3/StockCutting/main.cpp
@@ -5,11 +5,28 @@
  */
 
 #include <iostream>
+#include "vector.h"
 #include "console.h"
 using namespace std;
 
+int cutStockRec(Vector<int> &requests, int stockLength, int index, int soFar)
+{
+    // Base case
+    if ((soFar + requests[index]) > stockLength) {
+        return 1;
+    } else {
+        
+    }
+}
+
+int cutStock(Vector<int> &requests, int stockLength)
+{
+    
+}
+
 int main()
 {
+    
     
     return 0;
 }
